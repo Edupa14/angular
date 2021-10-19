62afed478fc713cbbe6e59b2c6946afdd4a5961d
Jair Paredes <jair7_14@hotmail.com>
2021-10-18 19:31:07 -0500

    feat: arrays, objetos e interfaces en Ts

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,19 +1,4 @@
-
-
-console.log('Hola Mundo!!!!');
-
 /*
     ===== Código de TypeScript =====
 */
 
-let nombre: string = 'Jair';
-
-nombre = 'Eduard';
-
-let hp: number | string = 123;
-
-hp = 'full';
-
-let estaVivo: boolean = true;
-
-console.log(nombre, hp, estaVivo);
